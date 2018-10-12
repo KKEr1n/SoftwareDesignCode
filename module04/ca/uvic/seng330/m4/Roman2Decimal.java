838db4991ec2bb1fd818704d57edc787b8629189
Neil Ernst <neil@neilernst.net>
2018-10-12 14:25:39 -0700

    some sample code for M4

---
+++ module04/ca/uvic/seng330/m4/Roman2Decimal.java
@@ -0,0 +1,13 @@
+package ca.uvic.seng330.m4;
+
+public class Roman2Decimal {
+  public Roman2Decimal(String x) {
+
+  }
+
+public int convert()
+{
+	// TODO Auto-generated method stub
+	return -1;
+}
+}
