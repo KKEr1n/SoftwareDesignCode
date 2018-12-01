66a293f3e7595b84cdf729046273448b6c8f000b
Neil Ernst <neil@neilernst.net>
2018-11-30 16:34:53 -0800

    add code for serialization module

---
+++ module10/ca/uvic/seng330/m10/SerializeCarTester.java
@@ -0,0 +1,25 @@
+// Original from http://horstmann.com/oodp3/slides/ch07/index.html#(44)
+package ca.uvic.seng330.m10;
+
+import java.io.*;
+
+/**
+   A program that serializes and deserializes a car.
+*/
+public class SerializeCarTester
+{
+   public static void main(String[] args)
+         throws IOException, ClassNotFoundException
+   {
+      Car beemer = new Car(100, 100, 60);
+      System.out.println(beemer);
+      ObjectOutputStream out = new ObjectOutputStream(
+            new FileOutputStream("fleet.dat"));
+      out.writeObject(beemer);
+      out.close();
+      ObjectInputStream in = new ObjectInputStream(
+            new FileInputStream("fleet.dat"));
+      System.out.println(in.readObject());
+      in.close();
+   }
+}
